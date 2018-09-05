72270acdffd963f69da081f1dd63d4219e455197
xueyong.zhang@ftdigital.com.cn <xueyong.zhang@ftdigital.com.cn>
2018-09-05 11:09:59 +0800

    change sty;e

--- pages/wheelprize/wheelprize.js
+++ pages/wheelprize/wheelprize.js
@@ -6,8 +6,8 @@ Page({
       { 'index': 3, 'name': '8元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-dest.png" },
       { 'index': 4, 'name': '10元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-share.png" },
       { 'index': 5, 'name': '20元话费', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-collect.png" },
-      { 'index': 6, 'name': '50元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-share.png" },
-      { 'index': 7, 'name': '100元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-dest.png" },
+      // { 'index': 6, 'name': '50元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-share.png" },
+      // { 'index': 7, 'name': '100元红包', 'icon': "https://mk-node-wxa.nihaomc.com/feimg/ind-dest.png" },
  
       ],//奖品列表
     isChance:false,//
@@ -56,35 +56,15 @@ Page({
     let iAwardsLength = aAwardsList.length||1,
        aAwardsItems = [],
       turnNum = 1 / iAwardsLength; 
-    let rotateDeg = 360 / iAwardsLength / 2 + 90;
-    let ctx = wx.createContext();
     for (let i = 0; i < iAwardsLength; i++) {
-      ctx.save();
-      ctx.beginPath();
-      ctx.translate(150, 150);
-      ctx.moveTo(0, 0);
-      ctx.rotate((360 / iAwardsLength * i - rotateDeg) * Math.PI / 180);
-      ctx.arc(0, 0, 150, 0, 2 * Math.PI / iAwardsLength, false);
-      if (i % 2 == 0) {
-        ctx.setFillStyle('#ffb820');
-      } else {
-        ctx.setFillStyle('#ffcb3f');
-      }
-      ctx.fill();
-      ctx.setLineWidth(0.5);
-      ctx.setStrokeStyle('#e4370e');
-      ctx.stroke();
-      ctx.restore();
+ 
       aAwardsItems.push({ turn: i*turnNum + 'turn', lineTurn:i*turnNum+turnNum/2 + 'turn',award:aAwardsList[i].name, icon: aAwardsList[i].icon});    
     }
     console.log("aAwardsItems-------------------", aAwardsItems);
     _This.setData({
       aAwardsItems: aAwardsItems
       });
-    wx.drawCanvas({
-      canvasId: 'lotteryCanvas',
-      actions: ctx.getActions()
-    })
+
   }
 
 })
